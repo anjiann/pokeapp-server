2a8bc6ebcab7c813eb1c8f55127a263e473e0781
Tasfia Tabassum <tasfiatabassum@tasfias-air.lan>
2021-01-11 15:44:41 -0500

    Search works

--- tempFunctions/search.js
+++ tempFunctions/search.js
@@ -1,33 +1,48 @@
+const arr = [];
+
 async function searchPokemon(e){
     e.preventDefault();
 
+    let input = document.getElementById("search-input").value
+
+    for (let i = 0; i <= arr.length; i++){
+        let table = document.getElementById("pokemonTable");
+        let row = table.insertRow(i+1);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+
+        //console.log(data.results[x].name;
+
+        let tmp = arr[i].startsWith(input);
+
+        if (tmp){
+
+
+            cell1.innerHTML = i;
+            cell2.innerHTML = arr[i];
+            console.log(arr[i]);
+        }
+    }
+
+}
+
+async function getAllPokemon(){
 	let res = await fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1118",{ 
 				method: "GET",
 				headers:{
 					"Content-Type": "application/json"
 				}
-	});
+    });
 
     const data = await res.json();
-    //console.log(data.results.name);
-    console.log(data.results[1].name);
-
-
-    let x = 0;
-
-    for (pokemon in data){
-        let table = document.getElementById("pokemonTable");
-        x = x + 1;
-        let row = table.insertRow(1);
-        let cell1 = row.insertCell(0);
-        let cell2 = row.insertCell(1);
-
-        console.log(data[pokemon].results[x].name)
-
-        cell1.innerHTML = x;
-        cell2.innerHTML = data[pokemon].results.name;
+    
+    for (let i = 0; i <= data.results.length - 1; i++){
+        arr.push(data.results[i].name);
     }
+
+    console.log("All pokemon saved in array");
 
 }
 
 document.getElementsByTagName("form")[0].addEventListener('submit', searchPokemon)
+document.onload = getAllPokemon();
