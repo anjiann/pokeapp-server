6fbfa5c5fad116ad917a6530edf3aa39fc043d5e
patelarj <ar.arjun2007@gmail.com>
2021-01-17 02:11:31 -0500

    bug fix

--- PokeAppServer/src/main/java/com/revature/controllers/TeamController.java
+++ PokeAppServer/src/main/java/com/revature/controllers/TeamController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,7 +38,7 @@ public class TeamController {
 	// takes in teams name and user id of the ream owner and creates the team 
 	
 	@PostMapping
-	public ResponseEntity<Teams> addTeam(Teams teams){
+	public ResponseEntity<Teams> addTeam(@RequestBody Teams teams){
 		
 		
 		return new ResponseEntity<Teams>(temImp.addTeam(teams), HttpStatus.CREATED);
