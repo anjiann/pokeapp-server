3f381b3bf017104359cf27d5ab493da7eea843cb
patelarj <ar.arjun2007@gmail.com>
2021-01-18 21:27:58 -0500

    cors fix

--- PokeAppServer/src/main/java/com/revature/config/CorsConfig.java
+++ PokeAppServer/src/main/java/com/revature/config/CorsConfig.java
@@ -11,11 +11,11 @@ public class CorsConfig implements WebMvcConfigurer{
 	public void addCorsMappings(CorsRegistry registry) {
 		// TODO Auto-generated method stub
 		registry.addMapping("/**") // all endpoints in the api
-        .allowedOrigins("http://localhost:1234", "http://localhost:3000", "*") // YOU MIGHT HAVE TO ADD OR CHANGE
+        .allowedOrigins("http://localhost:1234", "http://localhost:3000", "http://ec2-35-183-119-246.ca-central-1.compute.amazonaws.com" ) // YOU MIGHT HAVE TO ADD OR CHANGE
         .allowedMethods("PUT", "DELETE", "GET", "OPTIONS", "POST", "PATCH")
-        .allowedHeaders("header1", "Content-Type", "Authorization", "auth", "user");//YOU MIGHT HAVE TO ADD HEADERS
-//        .exposedHeaders("header1", "header2")
-   //    .allowCredentials(true);
+        .allowedHeaders("header1", "Content-Type", "Authorization", "auth", "user", "favorite", "team" );//YOU MIGHT HAVE TO ADD HEADERS
+       //.exposedHeaders("header1", "header2")
+   // .allowCredentials(true);
 	}
 	
 	
